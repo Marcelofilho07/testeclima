bfa73ed1d514b42610d5be8104956af62531d8d8
Your Name <Marcelofilho1987@gmail.com>
2018-12-02 21:44:35 -0200

    pfvr funciona

---
+++ chatbotclima.py
@@ -0,0 +1,63 @@
+import os
+import json
+import traceback
+from flask import Flask, request
+
+token = os.environ.get('FB_ACCESS_TOKEN')
+api_key = os.environ.get('WEATHER_API_KEY')
+app = Flask(__name__)
+
+def location_quick_reply(sender):
+    return {
+        "recipient": {
+            "id": sender
+        },
+        "message": {
+            "text": "Compartilhe sua localização:",
+            "quick_replies": [
+                {
+                    "content_type": "location",
+                }
+            ]
+        }
+    }
+
+@app.route('/', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.data.decode())
+            print(data)
+            message = data['entry'][0]['messaging'][0]['message']
+            sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
+            if 'attachments' in message:
+                if 'payload' in message['attachments'][0]:
+                    if 'coordinates' in message['attachments'][0]['payload']:
+                        location = message['attachments'][0]['payload']['coordinates']
+                        latitude = location['lat']
+                        longitude = location['long']
+            else:
+				url = 'http://api.openweathermap.org/data/2.5/weather?' \
+				'lat={}&lon={}&appid={}&units={}&lang={}'.format(latitude, longitude, api_key, 'metric', 'pt')
+				r = requests.get(url)
+				description = r.json()['weather'][0]['description'].title()
+				icon = r.json()['weather'][0]['icon']
+				weather = r.json()['main']
+				text_res = '{}\n' \
+							'Temperatura: {}\n' \
+							'Pressão: {}\n' \
+							'Humidade: {}\n' \
+							'Máxima: {}\n' \
+							'Mínima: {}'.format(description, weather['temp'], weather['pressure'], weather['humidity'], weather['temp_max'], weather['temp_min'])
+				payload = {'recipient': {'id': sender}, 'message': {'text': text_res}}
+				r = requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
+
+    elif request.method == 'GET':
+        if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
+            return request.args.get('hub.challenge')
+        return "Wrong Verify Token"
+    return "Nothing"
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
