8e57f9804939b710ed735c238fce2f6cc49454a9
Marcelofilho07 <43945868+Marcelofilho07@users.noreply.github.com>
2018-12-05 07:32:46 -0200

    Add files via upload

--- chatbotclima.py
+++ chatbotclima.py
@@ -6,63 +6,37 @@ import requests
 from cities_list import CITIES
 from flask import Flask, request
 
-token = os.environ.get('FB_ACCESS_TOKEN')
-api_key = os.environ.get('WEATHER_API_KEY')
+token = os.environ.get('FB_ACCESS_TOKEN') #FB_ACCESS_TOKEN é definida no Heroku
+api_key = os.environ.get('WEATHER_API_KEY') #WHEATER_API_KEY é definida no Heroku
 app = Flask(__name__)
 
+#Essa função envia ao usuário a solicitação da localização do mesmo
 def location_quick_reply(sender):
-    return {
-        "recipient": {
-            "id": sender
-        },
-        "message": {
-            "text": "Compartilhe sua localização:",
-            "quick_replies": [
-                {
-                    "content_type": "location",
-                }
-            ]
-        }
-    }
+    return { "recipient": { "id": sender},"message": {"text": "Compartilhe sua localização:","quick_replies": [{"content_type": "location",}]}}
 
-'''def send_attachment(sender, type, payload):
-    return {
-        "recipient": {
-            "id": sender
-        },
-        "message": {
-            "attachment": {
-                "type": type,
-                "payload": payload,
-            }
-        }
-    }'''
+#Essa função envia ao usuário uma mensagem
+def send_message(payload):
+    requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
 
-def send_attachment(sender, payload):
-    return {
-        "recipient": {
-            "id": sender
-        },
-        "message": {
-            "text": payload
-        }
-    }
-
+#Essa função envia as informações sobre o clima para o usuário
 def send_weather_info(sender, **kwargs):
     latitude = kwargs.pop('latitude', None)
     longitude = kwargs.pop('longitude', None)
     city_name = kwargs.pop('city_name', None)
 
+    #Checa se o query de busca sera por latitude e longitude ou pelo nome da cidade
     if latitude and longitude:
         query = 'lat={}&lon={}'.format(latitude, longitude)
     elif city_name:
         query = 'q={},br'.format(city_name)
 
+    #URL que disponibiliza o json com informações sobre o clima
     url = 'http://api.openweathermap.org/data/2.5/weather?' \
           '{}&appid={}&units={}&lang={}'.format(query,api_key,'metric','pt')
 
     r = requests.get(url)
 
+    #Alocando informações do clima em variáveis e enviando para o usuário
     description = r.json()['weather'][0]['description'].title()
     weather = r.json()['main']
     wind = r.json()['wind']
@@ -73,60 +47,27 @@ def send_weather_info(sender, **kwargs):
            'Máxima: {} °C\n' \
            'Mínima: {} °C\n' \
            'Velocidade do vento: {} km/h\n'.format(description, weather['temp'], weather['pressure'], weather['humidity'], weather['temp_max'], weather['temp_min'], wind['speed'])
-    '''if 'visibility' in weather:
-            weather_data = 'Visibilidade: {}'.format(weather['visibility'])'''
+
     payload = {'recipient': {'id': sender}, 'message': {'text': weather_data}}
-    '''response = r.json()
-
-    print (response)
-
-    name = response['name']
-    weather = response['main']
-    wind = response['wind']
-
-    elements = [{
-        'title': name,
-        'subtitle': 'Temperatura: {} graus'.format(str(weather['temp']).replace('.',',')),
-    }]
-
-    for info in response['weather']:
-        description = info['description'].capitalize()
-        icon = info['icon']
-
-        weather_data = 'Umidade: {}%\n' \
-                       'Pressão: {}\n' \
-                       'Velocidade do vento: {}'.format(weather['humidity'],
-                                                          weather['pressure'],
-                                                          wind['speed'])
-
-        if 'visibility' in response:
-            weather_data = '{}\n Visibilidade: {}'.format(weather_data, response['visibility'])
-
-        elements.append({
-            'title': description,
-            'subtitle': weather_data,
-            'image_url': 'http://openweathermap.org/img/w/{}.png'.format(icon)
-        })
-
-    #payload = send_attachment(sender,'template', {"template_type": "list", "top_element_style": "large", "elements": elements,})
-    payload = send_attachment(sender, elements)'''
+    
+    print(payload)
     send_message(payload)
 
     return None
 
-def send_message(payload):
-    requests.post('https://graph.facebook.com/v2.6/me/messages/?access_token=' + token, json=payload)
 
-
+#Função root do Flask, rota que se comunica com o webhook do Facebook
 @app.route('/', methods=['GET', 'POST'])
 def webhook():  
     if request.method == 'POST':
         try:
             data = json.loads(request.data.decode())
-            sender = data['entry'][0]['messaging'][0]['sender']['id'] # Sender ID
-            print(data['entry'][0]['messaging'][0])
+            sender = data['entry'][0]['messaging'][0]['sender']['id']
+            print(data)
+
             if 'message' in data['entry'][0]['messaging'][0]:
                 message = data['entry'][0]['messaging'][0]['message']
+
             else:
                 message = 'null'
 
@@ -137,14 +78,16 @@ def webhook():
                         latitude = location['lat']
                         longitude = location['long']
                         send_weather_info(sender, latitude=latitude, longitude=longitude)
+
             elif message != 'null':
 
                 text = '{}'.format(message['text'])
-                
+                flag = 0
                 for city in CITIES:
-                    if text.lower() in city:
+                    if text.lower() in city and flag == 0:
                         send_weather_info(sender, city_name=text)
-                        
+                        flag = 1
+
                 payload = location_quick_reply(sender)
                 send_message(payload) 
                 
@@ -152,11 +95,13 @@ def webhook():
         except Exception as e:
             print(traceback.format_exc())
 
+    #Verificação do Webhook
     elif request.method == 'GET':
         if request.args.get('hub.verify_token') == os.environ.get('FB_VERIFY_TOKEN'):
             return request.args.get('hub.challenge')
         return "Wrong verify_token"
-    return "Nothing"
+
+    return "Emptiness"
 
 
 if __name__ == '__main__':
